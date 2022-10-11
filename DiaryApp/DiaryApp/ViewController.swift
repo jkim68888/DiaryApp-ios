2ed55821eb279db6add6472633508b35f23d2c4b
Ethan-mo <102236847+Ethan-mo@users.noreply.github.com>
2022-10-12 00:47:50 +0900

    🎨 [style] 1. UI 제작 : Home, Detail, Setting View 구현

--- DiaryApp/DiaryApp/ViewController.swift
+++ DiaryApp/DiaryApp/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
-	override func viewDidLoad() {
+    @IBOutlet weak var homeInfoView: UIView!
+    @IBOutlet weak var homeCollectionView: UICollectionView!
+    override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		homeViewSetting()
 	}
+    func homeViewSetting(){
+        homeInfoView.backgroundColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
+        homeInfoView.clipsToBounds = true
+        homeInfoView.layer.cornerRadius = 15
+    }
 
 
 }
-
+extension ViewController: UICollectionViewDataSource{
+    // 컬렉션 뷰 내부의 셀의 개수를 지정한다.
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    // 컬렉션 뷰 내부의 셀의 내용을 세팅하는 부분, 여기서 직접 주거나, Cell파일에서 didSet(속성감시자)을 통해 초기세팅을 줄 수 있다.
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+}
+//extension ViewController: UICollectionViewDelegate{
+//    // Collection의 항목을 선택 했을 경우, 발생하는 이벤트
+//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        <#code#>
+//    }
+//}
