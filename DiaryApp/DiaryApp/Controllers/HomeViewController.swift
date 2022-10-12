d9a5501308ca2823cd5e32abf92857324dd52030
Ethan-mo <102236847+Ethan-mo@users.noreply.github.com>
2022-10-12 10:59:06 +0900

    🎨 [style] 1. UI 제작 : Home, Detail storyboard 분할 구현

---
+++ DiaryApp/DiaryApp/Controllers/HomeViewController.swift
@@ -0,0 +1,39 @@
+//
+//  HomeViewController.swift
+//  DiaryApp
+//
+//  Created by 모상현 on 2022/10/12.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var homeInfoView: UIView!
+    @IBOutlet weak var homeCollectionView: UICollectionView!
+    @IBOutlet weak var defaultStackView: UIStackView!
+    @IBOutlet weak var addPostBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setView()
+        // Do any additional setup after loading the view.
+    }
+    func setView(){
+        homeInfoView.clipsToBounds = true
+        homeInfoView.layer.cornerRadius = 20
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func addPost(_ sender: UIButton) {
+    }
+}
