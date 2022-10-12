b7f5b15733d7ad1c929461403c6a6038e978b1f5
Ethan-mo <102236847+Ethan-mo@users.noreply.github.com>
2022-10-13 01:57:09 +0900

    ✨[feat] + 🎨 [style] 1. DetailPostEdit, DPE_VC에서 이미지선택 시, 갤러리에서 이미지를 불러오는 기능 추가 2. DPE_VC에서 날짜를 Click 시, DatePicker를 불러오는 기능 추가 3. 기타 UI 포맷 변경

--- DiaryApp/DiaryApp/Controllers/Detail/DetailPostEditViewController.swift
+++ DiaryApp/DiaryApp/Controllers/Detail/DetailPostEditViewController.swift
@@ -6,24 +6,65 @@
 //
 
 import UIKit
+import PhotosUI
 
 class DetailPostEditViewController: UIViewController {
 
+    @IBOutlet weak var detailPostEditImageView: UIImageView!
+    @IBOutlet weak var detailPostEditDateLabel: UILabel!
+    @IBOutlet weak var detailPostEditDatePicker: UIDatePicker!
+    @IBOutlet weak var detailPostEditStackView: UIStackView!
+    @IBOutlet weak var detailPostEditDateView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        제스처부여하기()
+        //디자인세팅()
+    }
+    func 디자인세팅(){
+        detailPostEditDateView.backgroundColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+    }
+    func 제스처부여하기(){
+        let tapGestureImageView = UITapGestureRecognizer(target: self, action: #selector(이미지를눌렀을때))
+        detailPostEditImageView.addGestureRecognizer(tapGestureImageView) // 제스처를 추가....
+        detailPostEditImageView.isUserInteractionEnabled = true // 유저와의 소통을 가능하게...
+
+        let tapGestureDateLabel = UITapGestureRecognizer(target: self, action: #selector(달력을눌렀을때))
+        detailPostEditDateLabel.addGestureRecognizer(tapGestureDateLabel) // 제스처를 추가....
+        detailPostEditDateLabel.isUserInteractionEnabled = true // 유저와의 소통을 가능하게...
+    }
+    @objc func 이미지를눌렀을때(){
+        ///피커뷰를 사용하기 위해서 configuration을 먼저 설정해준다.
+        var configuration = PHPickerConfiguration()
+        configuration.selectionLimit = 1 //사진을 무한대로 가지고 올 수 있는... 1이면 1개만, 2는 2개만 가져온다.
+        configuration.filter = .any(of: [.images, .videos]) // 지금은 사진과 비디오를 가져올 수 있게 설정하였다.
+        /// 기본 설정을 가지고, 피커뷰컨트롤러 생성
+        let picker = PHPickerViewController(configuration: configuration)
+        /// 피커뷰 컨트롤러의 대리자 설정
+        picker.delegate = self
+        /// 피커뷰 띄우기
+        self.present(picker, animated: true,completion: nil)
+    }
+    @objc func 달력을눌렀을때(){
+        detailPostEditStackView.isHidden = true
+        detailPostEditDateView.isHidden = false
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+extension DetailPostEditViewController: PHPickerViewControllerDelegate{
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true)
+        let itemProvider = results.first?.itemProvider
+        if let itemProvider = itemProvider, itemProvider.canLoadObject(ofClass: UIImage.self){
+            itemProvider.loadObject(ofClass: UIImage.self) { (image, error) in
+                DispatchQueue.main.async {
+                    self.detailPostEditImageView.image = image as? UIImage
+                }
+            }
+        }else {
+            print("이미지를 불러올 수 없습니다.")
+        }
     }
-    */
-
+    
+    
 }
