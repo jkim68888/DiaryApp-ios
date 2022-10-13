bbdeb73d2709b4508b94676a506c634563f771d8
jkim68888 <jkim68888@gmail.com>
2022-10-13 16:51:37 +0900

    [Settings] 프로젝트 구조 변경 - MVVM패턴
    - 스토리보드, 뷰컨 네이밍 일부 변경

--- DiaryApp/DiaryApp/Scene/Post/ViewController/PostViewController.swift
+++ DiaryApp/DiaryApp/Scene/Post/ViewController/PostViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class DetailPostViewController: UIViewController {
+class PostViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- DiaryApp/DiaryApp/Scene/Signin/ViewController/SignInViewController.swift
+++ DiaryApp/DiaryApp/Scene/Signin/ViewController/SignInViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ProfileSocialSignInViewController: UIViewController {
+class SignInViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
