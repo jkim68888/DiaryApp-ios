97788857a6463895557e0d610226a84101fb8fb2
Ethan-mo <102236847+Ethan-mo@users.noreply.github.com>
2022-10-13 14:11:37 +0900

    🎨[style]  + ✨[feat] 1. Calendar + VC 제작완료, 2. Home에서 Calendar로 넘어가는 구조를 위해 제스처 기능 추가

--- DiaryApp/DiaryApp/Controllers/Home/HomeViewController.swift
+++ DiaryApp/DiaryApp/Controllers/Home/HomeViewController.swift
@@ -12,12 +12,14 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var homeInfoView: UIView!
     @IBOutlet weak var homeCollectionView: UICollectionView!
     @IBOutlet weak var defaultStackView: UIStackView!
+    @IBOutlet weak var calendarImage: UIImageView!
     @IBOutlet weak var addPostBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //navigationController?.setNavigationBarHidden(true, animated: false)
         setView()
+        제스처부여하기()
         // Do any additional setup after loading the view.
     }
     
@@ -26,15 +28,14 @@ class HomeViewController: UIViewController {
         homeInfoView.layer.cornerRadius = 20
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func 제스처부여하기(){
+        let tapGestureImageView = UITapGestureRecognizer(target: self, action: #selector(달력이미지를눌렀을때))
+        calendarImage.addGestureRecognizer(tapGestureImageView) // 제스처를 추가....
+        calendarImage.isUserInteractionEnabled = true // 유저와의 소통을 가능하게...
     }
-    */
+    @objc func 달력이미지를눌렀을때(){
+        performSegue(withIdentifier: "toCalendar", sender: nil)
+    }
 
     @IBAction func addPost(_ sender: UIButton) {
         performSegue(withIdentifier: "toDetailPost", sender: nil)
