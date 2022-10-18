0a4bb706ad78cd5ccca0007fd01584459bc14a0c
Ethan-mo <102236847+Ethan-mo@users.noreply.github.com>
2022-10-18 10:57:39 +0900

    🎨[style] 1. 약간의 UI Frame변경

--- DiaryApp/DiaryApp/Scene/PostVeiwer/ViewController/PostViewerViewController.swift
+++ DiaryApp/DiaryApp/Scene/PostVeiwer/ViewController/PostViewerViewController.swift
@@ -9,10 +9,25 @@ import UIKit
 
 class PostViewerViewController: UIViewController {
 
+    var tempPostData: TempPost?
+    
+    @IBOutlet weak var postViewerImageView: UIImageView!
+    @IBOutlet weak var postViewerDateLabel: UILabel!
+    @IBOutlet weak var postViewerTitleLabel: UILabel!
+    @IBOutlet weak var postViewerDescriptionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    func setPost(){
+//        guard var tempPostData = tempPostData else {return}
+//        postViewerImageView.image = tempPostData.postImage
+//        postViewerDateLabel.text = tempPostData.createDate
+//        postViewerTitleLabel.text = tempPostData.postTitle
+//        postViewerDescriptionLabel.text = tempPostData.postScript
+
     }
     
 
