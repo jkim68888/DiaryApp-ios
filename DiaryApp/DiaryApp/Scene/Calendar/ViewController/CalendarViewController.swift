c6875367c820535912279055e04a33e69ea4b16e
jkim68888 <jkim68888@gmail.com>
2022-10-18 18:05:21 +0900

    [feat + style] 네비게이션바 구현
    - 네비게이션바 타이틀 관련 스타일 extension 구현
    - 네비게이션바 back 버튼 함수 utils에 구현
    - Settings뷰 & calendar뷰 에 적용
    - 관련 주석 작성

--- DiaryApp/DiaryApp/Scene/Calendar/ViewController/CalendarViewController.swift
+++ DiaryApp/DiaryApp/Scene/Calendar/ViewController/CalendarViewController.swift
@@ -11,19 +11,11 @@ class CalendarViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+		setUI()
+		customBackButton(self: self, target: self.navigationController!)
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+	func setUI() {
+		self.title = "캘린더"
+	}
 }
