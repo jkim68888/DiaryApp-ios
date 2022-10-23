fea44b96252c497f09e0ffb31c6d27863ed67da5
jkim68888 <jkim68888@gmail.com>
2022-10-23 10:36:16 +0900

    [add] 카카오 sdk 설치 및 init

--- DiaryApp/DiaryApp/AppDelegate.swift
+++ DiaryApp/DiaryApp/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import KakaoSDKCommon
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,6 +14,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		// 네비게이션바 공통 스타일
 		UINavigationBar.appearance().customNavigationBar()
+		
+		
+		// kakao init
+		KakaoSDK.initSDK(appKey: Config().kakaoAppKey)
+		
 		return true
 	}
 
