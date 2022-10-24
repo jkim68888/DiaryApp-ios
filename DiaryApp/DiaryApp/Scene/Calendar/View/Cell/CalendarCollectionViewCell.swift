be7a16ca78bf28281036f802f35b2ee3b3631a46
Ethan-mo <102236847+Ethan-mo@users.noreply.github.com>
2022-10-24 15:18:54 +0900

    Commit Test

---
+++ DiaryApp/DiaryApp/Scene/Calendar/View/Cell/CalendarCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CalendarCollectionViewCell.swift
+//  DiaryApp
+//
+//  Created by 모상현 on 2022/10/24.
+//
+
+import UIKit
+
+class CalendarCollectionViewCell: UICollectionViewCell {
+    
+}
